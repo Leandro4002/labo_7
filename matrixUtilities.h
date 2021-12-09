3763691312c987bcd5afdd0de4f33bdd59ec7d19
Miguel <miguel.jalube@gmail.com>
2021-12-09 17:40:32 +0100

    comments in .h

--- matrixUtilities.h
+++ matrixUtilities.h
@@ -27,46 +27,61 @@ using Matrix = std::vector<VInt>;
  * @return  boolean true if the matrix is square
  */
 bool isSquare(const Matrix& matrix);
+
 /**
  * Check if all the lines in a matrix have the same number of columns
  * @param   matrix the matrix to check
  * @return  boolean true if regular
  */
 bool isRegular(const Matrix& matrix);
+
 /**
- *
- * @param matrix
- * @return
+ * Get the size of the smallest column
+ * @param   matrix
+ * @return  the size of the smallest column
  */
 std::size_t minColumn(const Matrix& matrix);
+
 /**
- *
+ * Returns a vector with the sum of each row of the matrix. The values are indexed
+ * in the same order as the matrix. Example
+ * m = { v1, v2, v3 }
+ * sumRow(m) // returns { sumV1, sumV2, sumV3 }
  * @param matrix
  * @return
  */
 VInt sumRow(const Matrix& matrix);
+
 /**
- *
- * @param matrix
- * @return
+ * Get the sum of the columns of the matrix and returns a vector with it
+ * @param   matrix
+ * @return  returns a vector with the addition of each column of the matrix
  */
 VInt sumColumn(const Matrix& matrix);
+
 /**
- *
+ * Get the row of the matrix with the lowest sum of elements. Example:
+ * m = { {0, 0}, {5, -3}, {2, 1} }
+ * vectSumMin(m) // returns {2, 1}
  * @param matrix
- * @return
+ * @return The row of the matrix that has the lowest sum
  */
 VInt vectSumMin(const Matrix& matrix);
+
 /**
- *
+ * Reorder the elements of the matrix randomly
  * @param matrix
  */
 void shuffleMatrix(Matrix& matrix);
+
 /**
- *
+ * Order matrix in ascending order of the first element of each row. Example :
+ * m = { {4, 4}, {1, 3}, {2} }
+ * sortMatrix(m) // m = { {1, 3}, {2}, {4, 4} }
  * @param matrix
  */
 void sortMatrix(Matrix& matrix);
+
 /**
  * Operator << for int vectors
  * @param os
@@ -74,6 +89,7 @@ void sortMatrix(Matrix& matrix);
  * @return
  */
 std::ostream& operator<< (std::ostream& os,const VInt& vector);
+
 /**
  * Operator << for matrix (matrix = vector<vector<int>>)
  * @param   os
